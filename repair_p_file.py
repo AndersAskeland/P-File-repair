69af41a9243239a34709c03c2fa6a448cbd99338
andersaskeland <anders.askeland@gmail.com>
2021-08-17 22:09:30 +0200

    [doc] Added additional documentation

--- repair_p_file.py
+++ repair_p_file.py
@@ -1,3 +1,16 @@
+# -----------------------------------------------------------------------------
+# Project: P-File repair
+#
+# What: A simple tool to repair certain GE p-files that are corrupted. This is specifically a problem for certain 
+# v. 28 p-files. These files always contains 10058420 bytes and contains a lot of unused 0 values. The following 
+# software removes these values and creates a new file.
+# 
+# Currently, the software only works with v. 28 files. If you encounter this problem with other files and wish to 
+# use this software, please submit a feature request.
+# 
+# Some code is adapted from the "spant" package (https://cran.r-project.org/web/packages/spant/index.html).
+#
+# ------------------------------------------------------------------------------
 # ------------------------------------------------------------------------------
 # 1 - Imports
 # ------------------------------------------------------------------------------
@@ -6,7 +19,7 @@ from PySide2.QtWidgets import QApplication
 from modules.user_interface import MainWindow
 
 # ------------------------------------------------------------- #
-# 5 - Application start                                         #
+# 2 - Application start                                         #
 # ------------------------------------------------------------- #
 if __name__ == "__main__":
     # Create application
