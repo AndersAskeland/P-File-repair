a6831ae4afcc9e84aa478e968fcadfb3004d6dd3
andersaskeland <anders.askeland@gmail.com>
2021-08-18 14:56:40 +0200

    [fix] Fixed pyinstaller

--- build_windows.bat
+++ build_windows.bat
@@ -1,20 +1,19 @@
-# Load packages
+:: Load packages
 pip install -r requirements.txt  # Requirments
 
-# Build UI
+:: Build UI
 pyside2-uic resources/user_interface/mainwindow.ui -o resources/user_interface/mainwindow.py
 pyside2-rcc resources/graphics/icons.qrc -o icons_rc.py
 
-# Create requirements
+:: Create requirements
 pip freeze > requirements.txt
 
-# Build file (pyinstaller)
+:: Build file (pyinstaller)
 pyinstaller --specpath "pyinstaller" ^
     --workpath "pyinstaller\build" ^
     --distpath "pyinstaller\dist" ^
-    --log-level=WARN ^
-    --add-data="..\resources\graphics:resources\graphics" ^
-    --windowed ^
+    --onefile ^
     --noconfirm ^
-    --icon=icon.ico ^
+    --noconsole ^
+    --icon=..\resources\graphics\icon.ico ^
     repair_p_file.py
